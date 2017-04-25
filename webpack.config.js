c0cabc68624d7923a6634cb6b0735868a817b794
StudentWan <benyuwan@gmail.com>
2017-04-26 00:21:26 +0800

    complete basic config

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,12 @@
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
     devtool: "eval-source-map",
 
     entry: __dirname + '/app/main.js',
     output: {
-        path: __dirname + '/public',
+        path: __dirname + '/build',
         filename: 'bundle.js'
     },
 
@@ -20,14 +23,30 @@ module.exports = {
             },
             {
                 test: /\.css$/,
-                loader: 'style-loader!css-loader?modules'
+                loader: 'style-loader!css-loader?modules!postcss'
             }
         ]
     },
 
+    plugins: [
+        new webpack.BannerPlugin("Copyright Flying Unicorns inc."),
+        new webpack.LoaderOptionsPlugin({
+            options: {
+                postcss: function () {
+                    return [precss, autoprefixer];
+                }
+            }
+        }),
+        new HtmlWebpackPlugin({
+            template: __dirname + '/app/index.tmpl.html'
+        }),
+        new webpack.HotModuleReplacementPlugin()
+    ],
+
     devServer: {
         contentBase: './public',
         historyApiFallback: true,
-        inline: true
+        inline: true,
+        hot: true
     }
 };
